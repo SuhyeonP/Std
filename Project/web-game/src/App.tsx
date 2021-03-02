1ef0265dc59ccdad42e697c1595f57d4063a63ea
suhyeonP <ahah12k@gmail.com>
2021-03-02 16:24:05 +0900

    add type

--- Project/web-game/src/App.tsx
+++ Project/web-game/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import RSP from "./com/rsp";
 import DDD from "./dd/lower2";
 
 function App() {
 
   return (
     <div className="App">
-      <RSP/>
       <DDD/>
     </div>
   );
--- Project/web-game/src/dd/lower2.tsx
+++ Project/web-game/src/dd/lower2.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const _SocialNetworks = [
     {title: "Twitter", color: "white", backgroundColor: "Red"},
@@ -14,26 +14,29 @@ const DDD = () => {
     const [ lists, setLists ] = React.useState(_SocialNetworks);
     const [ grab, setGrab ] = React.useState<any>()
 
-    const _onDragOver = (e:any)=> {
+    const _onDragOver = (e:React.DragEvent<HTMLLIElement>)=> {
         e.preventDefault();
     }
 
-    const _onDragStart = (e:any) => {
-        setGrab(e.target);
-        e.target.classList.add("grabbing");
+    const _onDragStart = (e:React.DragEvent<HTMLLIElement>) => {
+        const tgt= e.target as HTMLLIElement
+        setGrab(tgt);
+        tgt.classList.add("grabbing");
         e.dataTransfer.effectAllowed = "move";
-        e.dataTransfer.setData("text/html", e.target);
+        e.dataTransfer.setData("text/html", "li");
     }
 
-    const _onDragEnd = (e:any) => {
-        e.target.classList.remove("grabbing");
+    const _onDragEnd = (e:React.DragEvent<HTMLLIElement>) => {
+        const tgt= e.target as HTMLLIElement
+        tgt.classList.remove("grabbing");
 
         e.dataTransfer.dropEffect = "move";
     }
 
-    const _onDrop = (e:any) => {
+    const _onDrop = (e:React.DragEvent<HTMLLIElement>) => {
+        const tgt= e.target as HTMLLIElement
         let grabPosition = Number(grab.dataset.position);
-        let targetPosition = Number(e.target.dataset.position);
+        let targetPosition = Number(tgt.dataset.position);
 
         let _list = [ ...lists ];
         _list[grabPosition] = _list.splice(targetPosition, 1, _list[grabPosition])[0];
